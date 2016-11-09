bbdace3aa009cb45af2c25fb6210e5b186919e00
LIQ1234 <418019596@qq.com>
2016-11-10 01:08:25 +0800

    this is new apple

--- Apple/js/index.js
+++ Apple/js/index.js
@@ -1,10 +1,105 @@
 $(function(){
+    var now=0,next=0,time=0;
+    var flag=true;
+    function move(){
+        next++;
+        if(next>=$(".banner>div").length){
+            next=0;
+            flag=false;
+        }
+        $(".banner>div").eq(now).animate({width:"80%",height:"80%",opacity:"0.5"}).css("zIndex",0);
+        $(".banner>div").eq(next).animate({left:0},function(){
+            $(".banner>div").eq(now).animate({left:"100%",width:"100%",height:"100%",opacity:"1"});
+            now=next;
+            time=0;
+            flag=true;
+        }).css("zIndex",1);
+    }
+    function move1(){
+        time+=50;
+        var bili=time/3000;
+        if(bili>1){bili=1};
+        $(".progress").eq(now).css({width:bili*100+"%"});
+        if(flag==false){
+            $(".progress").css("width",0);
+        }
+    }
+    var t1=setInterval(move,3000);
+    var t2=setInterval(move1,50);
+    $(window).focus(function(){
+        t1=setInterval(move,3000);
+        t2=setInterval(move1,50);
+    })
+    $(window).blur(function(){
+        clearInterval(t1,t2);
+    })
+
+    $(".point>li").click(function(){
+        next=$(this).index(".point>li");
+        stop();
+    })
+    $(".pre").click(function(){
+        next--
+        if(next==-1){
+            next=2;
+        }
+        stop();
+    })
+    $(".next").click(function(){
+        next++
+        if(next==3){
+            next=0;
+        }
+        stop();
+    })
+    function stop(){
+        /*定时器停掉*/
+        clearInterval(t1);
+        clearInterval(t2);
+
+        /*按钮的变化*/
+        $(".point>li").find(".progress").css("width",0);
+        $(".point>li").eq(next).find(".progress").css("width","100%");
+
+        /*轮播图发生变化*/
+        if(next>now){
+            $(".banner>div:eq("+now+")").animate({width:"80%",height:"80%"}).css("zIndex",0);
+
+            $(".banner>div:eq("+next+")").animate({left:0},function(){
+                $(".banner>div:eq("+now+")").css({
+                    left:"100%",width:"100%",height:"100%"
+                })
+                now=next;
+
+            }).css("zIndex",1)
+        }else{
+            $(".banner>div:eq("+now+")").animate({left:"100%"}).css("zIndex",1);
+            $(".banner>div").eq(next).css({
+                width:"80%",height:"80%",left:0
+            }).animate({width:"100%",height:"100%"},function(){
+                now=next;
+            })
+
+
+        }
+    }
+
+
+    window.onresize=function(){
+        if($(window).width()<=735){
+            $(".small_nav").hide();
+        }
+    }
+    $(".footcen label").on("click",function(){
+        $(this).siblings(".foot_son").toggle(500);
+    })
+
     $(".menu_open").on("click",function(){
         $(".small_nav").slideDown(1000);
-        $(".header").hide();
-        $(".banner").hide();
-        $(".pic").hide();
-        $("footer").hide();
+        // $(".header").hide(1000);
+        // $(".banner").hide();
+        // $(".pic").hide();
+        // $("footer").hide();
     })
     $(".menu_close").on("click",function(){
         $(".small_nav").slideUp(1000);
@@ -12,8 +107,5 @@ $(function(){
         $(".banner").show();
         $(".pic").show();
         $("footer").show();
-    })
-    $(".footcen label").on("click",function(){
-        $(this).siblings(".foot_son").toggle(500);
     })
 })
